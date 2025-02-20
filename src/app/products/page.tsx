82889e92a0c973f954af6626db3eebb3658a418f
RodrigoNet0 <rodrigonetogithub@gmail.com>
2025-02-20 01:37:31 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,7 @@
+import { Label } from "@radix-ui/react-label";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@radix-ui/react-label";
 
 
 
