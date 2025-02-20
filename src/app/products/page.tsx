7a3da5b7fcdd3b8c503ff9b0a5c8fe97e3f90d47
RodrigoNet0 <rodrigonetogithub@gmail.com>
2025-02-20 01:19:04 -0300

    feat: add shadcn, initial setup

---
+++ src/app/products/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+
+
+
+
+const ProductsPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center bg-black  border-rounded border-2 border-red-500 p-10">
+      Products Page
+      <Button>Botão do guguete</Button> 
+      <Label className="text-red-500">Label do guguete</Label>
+      <Input/>
+    </div>
+  );
+};
+
+export default ProductsPage;
